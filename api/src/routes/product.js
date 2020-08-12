ca0e7e4feebfd4cea0e1cf4c3dbf6a9573d00314
Mariano <marianomotos50@gmail.com>
2020-08-12 14:50:28 -0300

    S21, S21, COMPLETE

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -9,4 +9,15 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+server.get('/:id', (req, res, next) => {
+	Product.findOne({
+		where: {
+			id: req.params.id
+		}
+	})
+	.then(product => {
+		res.send(product)
+	})
+})
+
 module.exports = server;
