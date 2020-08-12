094ed11e18bda3315eda23bd59be1480b513b50e
Agustina Rojas <agustinarojas2214@gmail.com>
2020-08-12 19:40:48 -0300

    t10, t11 y t13

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,8 @@ import React, {useState} from 'react';
 import {Route} from 'react-router-dom';
 // import './App.css';
 import Catalogo from './Components/Catalogo/Catalogo';
-import axios from 'axios';
-
-const api = axios.create({baseURL: 'http://localhost:3001/products'});
 
 function App() {
-	(function () {
-		console.log('hi');
-		api.get('/').then(res => console.log(res));
-	})();
 	const [products, setProducts] = useState([
 		{imagen: null, titulo: 'jueguete', precio: '$400', id: 1},
 		{imagen: null, titulo: 'otro jueguete', precio: '$500', id: 2},
