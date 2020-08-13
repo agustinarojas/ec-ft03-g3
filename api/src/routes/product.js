dd93475db0583d21af342752092c27d1ebd83541
Agustina Rojas <agustinarojas2214@gmail.com>
2020-08-13 12:14:31 -0300

    c

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,5 @@
 const server = require('express').Router();
-const { Product } = require('../db.js');
+const {Product} = require('../db.js');
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
