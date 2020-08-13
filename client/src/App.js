137b3a4a3158e31305f8cd83e2ca6428e7c7ce1e
Agustina Rojas <agustinarojas2214@gmail.com>
2020-08-13 11:46:21 -0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Route} from 'react-router-dom';
-// import './App.css';
+import axios from 'axios';
 import Catalogo from './Components/Catalogo/Catalogo';
 
 function App() {
 	const [products, setProducts] = useState([
 		{imagen: null, titulo: 'jueguete', precio: '$400', id: 1},
 		{imagen: null, titulo: 'otro jueguete', precio: '$500', id: 2},
-		{imagen: null, titulo: 'jueguete', precio: '$400', id: 1},
-		{imagen: null, titulo: 'otro jueguete', precio: '$500', id: 2},
+		{imagen: null, titulo: 'jueguete', precio: '$400', id: 3},
+		{imagen: null, titulo: 'otro jueguete', precio: '$500', id: 4},
 	]);
+	useEffect(() => {
+		axios
+			.get('http://localhost:3000/products')
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
+	});
 	return (
 		<div className="App">
 			<Route path="/" render={() => <Catalogo products={products} />} />
