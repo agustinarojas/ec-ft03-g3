d9d55336bdd334b036bbe752cef9b04327916a31
Fran Troncoso <64669949+Fran-3xo@users.noreply.github.com>
2020-08-14 17:03:24 -0300

    agregado de funcion

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,41 @@ import React, {useState, useEffect} from 'react';
 import {Route} from 'react-router-dom';
 import axios from 'axios';
 import SearchBar from './Components/SearchBar/SearchBar';
-import Catalogo from './Components/Catalogo/Catalogo';
+import Catalogo from './Components/catalogo/Catalogo';
 import Products from './Components/producto';
+import Form from './Components/Form/Form';
 
 function App() {
-  const [products, setProducts] = useState([
-    { imagen: null, titulo: 'jueguete', precio: '$400', id: 1 },
-    { imagen: null, titulo: 'otro jueguete', precio: '$500', id: 2 },
-  ]);
-  useEffect(() => {
-    axios
-      .get('http://localhost:3000/products')
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
-  });
-  return (
-    <div className="product">
-      <Route path="/" render={() => <Catalogo products={products} />} />
-      <Route
-        path="/product/:id"
-        render={({ match }) => <Products producto={match.params.id} />}
-      />
-    </div>
-  );
-  }
-  function filterCat (categoria) {
+	const [products, setProducts] = useState([]);
+	const [buscar, setBuscar] = useState('');
+	const apiRequest = buscar => {
+		let url = buscar ? `search?valor=${buscar}` : 'products';
+		axios
+			.get(`http://localhost:3005/${url}`)
+			.then(res => setProducts(res.data))
+			.catch(err => console.log(err));
+	};
+	useEffect(() => {
+		apiRequest(buscar);
+	}, [buscar]);
+
+	const search = input => {
+		setBuscar(input);
+	};
+	return (
+		<div className="product">
+			<SearchBar search={search} />
+			<Form />
+			<Route path="/" render={() => <Catalogo products={products} />} />
+			<Route path="/product/:id" render={({match}) => <Products producto={match.params.id} />} />
+		</div>
+	);
+}
+function filterCat (categoria) {
 	axios.get (`http://localhost:3005/products/categorias/${categoria}`)
 	.then(res => {
 		setProducts(res.data)
 	})
 	.catch(err)
 }
-//ACOMODAR ESA FUNCION
 export default App;
