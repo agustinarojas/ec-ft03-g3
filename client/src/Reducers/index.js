b60ced6d2a6f18985e3435ad4495ed252803f4f2
franco troncoso <frantronco11@gmail.com>
2020-08-18 18:36:13 -0300

    modelos nuevos

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -1,41 +1,45 @@
-
-import {GET_PRODUCTS, POST_CATEGORY, PUT_CATEGORY, POST_PRODUCTS, PUT_PRODUCTS} from '../Constants/ProductsConstants';
+import {
+  GET_PRODUCTS,
+  POST_CATEGORY,
+  PUT_CATEGORY,
+  POST_PRODUCTS,
+  PUT_PRODUCTS,
+} from "../Constants/ProductsConstants";
 const inicialState = {
-	products: [],
-	category: {},
-	putCat: {}
+  products: [],
+  category: {},
+  putCat: {},
   product: {},
-  putProduct: {}
+  putProduct: {},
 };
-function rootReducer (state = initialState, action) {
-  switch(action.type){
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
     case GET_PRODUCTS:
-    return {
-      ...state,
-      products: action.products
-    }
+      return {
+        ...state,
+        products: action.products,
+      };
     case POST_PRODUCTS:
-    return {
-      ...state,
-      product: action.product
-    }
+      return {
+        ...state,
+        product: action.product,
+      };
     case PUT_PRODUCTS:
-    return {
-      ...state,
-      putProduct: action.putProduct
-    }
-      case POST_CATEGORY:
-				return {
-					...state,
-					category: state.category
-				}
-				case PUT_CATEGORY:
-					return {
-						...state,
-						putCat: action.putCat
-					}
+      return {
+        ...state,
+        putProduct: action.putProduct,
+      };
+    case POST_CATEGORY:
+      return {
+        ...state,
+        category: state.category,
+      };
+    case PUT_CATEGORY:
+      return {
+        ...state,
+        putCat: action.putCat,
+      };
   }
   return state;
-
 }
 export default rootReducer;
