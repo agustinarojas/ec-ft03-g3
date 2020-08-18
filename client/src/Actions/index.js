bf8a9a11bfb641a8dd4d475536cafd600bef10e2
Agustina Rojas <agustinarojas2214@gmail.com>
2020-08-18 17:44:37 -0300

    conflictos

--- client/src/Actions/index.js
+++ client/src/Actions/index.js
@@ -1,33 +1,34 @@
 import axios from 'axios';
-import { GET_PRODUCTS, POST_PRODUCTS, PUT_PRODUCTS } from '../Constants/ProductsConstants.js'
+import {GET_PRODUCTS, POST_PRODUCTS, PUT_PRODUCTS} from '../Constants/ProductsConstants.js';
+
 export function getProducts() {
-  return function(dispatch) {
-    return axios
+	return function (dispatch) {
+		return axios
 			.get('http://localhost:3005/products')
-      .then(res => {
-        dispatch({type: GET_PRODUCTS, products:res.data})
-      })
+			.then(res => {
+				dispatch({type: GET_PRODUCTS, products: res.data});
+			})
 			.catch(err => console.log(err));
 	};
 }
 
 export function postProducts(product) {
-  return function(dispatch) {
-    return axios
-      .post('http://localhost:3005/products', product)
-      .then(res => {
-        dispatch({type: POST_PRODUCTS, product})
-      })
+	return function (dispatch) {
+		return axios
+			.post('http://localhost:3005/products', product)
+			.then(res => {
+				dispatch({type: POST_PRODUCTS, product});
+			})
 			.catch(err => console.log(err));
 	};
 }
-    export function putProducts(product,id) {
-      return function(dispatch) {
-        return axios
-          .put(`http://localhost:3005/products/${id}`, product)
-          .then(res => {
-            dispatch({type: PUT_PRODUCTS, product})
-          })
-    			.catch(err => console.log(err));
-    	};
-  }
+export function putProducts(product, id) {
+	return function (dispatch) {
+		return axios
+			.put(`http://localhost:3005/products/${id}`, product)
+			.then(res => {
+				dispatch({type: PUT_PRODUCTS, product});
+			})
+			.catch(err => console.log(err));
+	};
+}
