552e9a73857b6ecba89c1c3547df0883ecf92c32
MarianoL <66218346+MakeNoise01@users.noreply.github.com>
2020-08-19 12:48:19 -0300

    css agregado

--- client/src/Components/product/producto.js
+++ client/src/Components/product/producto.js
@@ -4,19 +4,21 @@ export default function Products(producto) {
 	return (
 		<div className="wrapper">
 			<div>
-				<img src={producto?.producto[0]?.imagen} />
+				<img src={producto?.producto[0]?.imagen} className='Imga' />
 			</div>
-			<h1>{producto?.producto[0]?.titulo}</h1>
-			<h3>$ {producto?.producto[0]?.precio}</h3>
-			<div>
-				<p>Descripción: {producto?.producto[0]?.descripcion}</p>
-			</div>
-			<h6>reviews</h6>
-			<div>
+			<div className='Description'>
+				<h1>{producto?.producto[0]?.titulo}</h1>
 				<div>
-					<p>Stock: {producto?.producto[0]?.stock}</p>
+					<p>{producto?.producto[0]?.descripcion}</p>
+					<div>
+						<p>Stock: {producto?.producto[0]?.stock}</p>
+					</div>
 				</div>
-				<button className="btn-info">Add to cart</button>
+				<h6>(reviews)</h6>
+				<div className='Precio'>
+					<h3>$ {producto?.producto[0]?.precio}</h3>
+					<button type="button" class="btn btn-primary">Add to cart</button>
+				</div>
 			</div>
 		</div>
 	);
