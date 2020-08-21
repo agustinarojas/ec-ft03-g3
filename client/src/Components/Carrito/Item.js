e59662f1e2dc2d4c520ef845f1f8924d779b24c7
Mariano <MakeNoise01@github.com>
2020-08-21 00:32:10 -0300

    Para mergear

--- client/src/Components/Carrito/Item.js
+++ client/src/Components/Carrito/Item.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './Item.css';
 
-export default function Item({products}) {
+export default function Item({ products }) {
 	return (
-		<div>
+		<div className='carritoItem'>
 			{products?.map(p => (
-				<ul className="list-group list-group-flush">
+				<ul className="list-group list-group-flush cartitem">
 					<li class="list-group-item disp">
-						<div>
-							<img src={p.imagen} />
+						<img className='imgCart' src={p.imagen} />
+						<div className='titdes'>
 							<p>{p.titulo}</p>
 							<p>{p.descripcion}</p>
 						</div>
+						
 						<button className="btn botoncart">-</button>
-						<p>{p.cantidad}</p>
+						<p>{p.cantidad} (cantida)</p>
 						<button className="btn botoncart">+</button>
 						<p>$ {p.precio} </p>
 						<button>X</button>
