c1d34eccfe6e12da09db4ef61311e483f5f2ff58
franco troncoso <frantronco11@gmail.com>
2020-08-27 15:04:57 -0300

    asd

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,7 +1,14 @@
 const server = require('express').Router();
 const {Op} = require('sequelize');
 const {User, Carrito, Product} = require('../db.js');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy
 
+passport.use(new Strategy(
+    function(email, password, done) {
+
+    }
+))
 server.get('/', (req, res) => {
 	User.findAll()
 		.then(users => {
